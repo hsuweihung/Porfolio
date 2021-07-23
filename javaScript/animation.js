88aa1de77feba5835ebfd6abec401481860b8016
william <william19970923@gmail.com>
2021-07-23 17:02:56 +0800

    commit layout style

--- javaScript/animation.js
+++ javaScript/animation.js
@@ -8,12 +8,12 @@ $(document).ready(function () {
 
     /* Check the location of each desired element */
     $('.reveal').each(function (i) {
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      let bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      let bottom_of_window = $(window).scrollTop() + $(window).height();
 
       /* If the object is completely visible in the window, fade it in */
       if (bottom_of_window > bottom_of_object) {
-        $(this).animate({ 'opacity': '1' }, 50);
+        $(this).animate({ 'opacity': '1' }, 10);
         /* 1500 is the duration of fade effect */
       }
     });
--- javaScript/scroll.js
+++ javaScript/scroll.js
@@ -16,20 +16,32 @@ function LandingPageScrollTrigger() {
         }
     })
     LandingPageScrollTrigger
-        /*.from('.landingPage #logo', {
-            opacity: 0, x: "-31%", duration: 2.2, ease: "sine.in",
-        }, 0)*/
-        .from('.landingPage .header .menu .anination_active a', {
-            opacity: 0, x: "70%", duration: 0.5, stagger: 0.2, ease: "sine.inOut",
-        }, 0.2)
         .from('.wrap .banner_txt .mb_top h1 span', {
-            opacity: 0, x: "-22%", duration: 1.0, ease: "sine.inOut",
+            opacity: 0, x: "-22%", duration: 0.8, ease: "sine.inOut",
         }, 0.4)
+        .from('.wrap .banner_work .mb_top h1 span', {
+            opacity: 0, x: "-22%", duration: 0.8, ease: "sine.inOut",
+        }, 0.4)
+        .from('.wrap .banner_work .work_intro span', {
+            opacity: 0, x: "-22%", duration: 0.5, stagger: 0.4, ease: "sine.inOut",
+        }, 0.8)
         .from('.wrap .banner_txt .mb_hidden_txt span', {
             opacity: 0, x: "-22%", duration: 0.5, stagger: 0.4, ease: "sine.inOut",
         }, 0.8)
         .from('.wrap .banner_txt .pc_hidden_txt .mb-text mb_sub_text span', {
             opacity: 0, x: "-22%", duration: 2.0, stagger: 0.4, ease: "sine.inOut",
+        }, 0.8)
+        .from('.about_img', {
+            opacity: 0, x: "-22%", duration: 0.8, ease: "sine.inOut",
+        }, 0.4)
+        .from('.about_txt h2', {
+            opacity: 0, x: "-22%", duration: 0.8, stagger: 0.4, ease: "sine.inOut",
+        }, 0.8)
+        .from('.about_txt span', {
+            opacity: 0, x: "-22%", duration: 0.8, stagger: 0.4, ease: "sine.inOut",
+        }, 0.8)
+        .from('.intro_me', {
+            opacity: 0, x: "-22%", duration: 0.8, stagger: 0.4, ease: "sine.inOut",
         }, 0.8)
 }
 
